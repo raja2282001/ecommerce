cfd4a0c6f70b28b145b957a00cae8183befe1fbe
parthpateltechein <parthpatel@techein.com>
2025-08-25 11:05:51 +0530

    change in app.js

--- app.js
+++ app.js
@@ -2,31 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
-const serverlessExpress = require("@vendia/serverless-express");
 
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Routers
-const userRouter = require("../Router/UserRouter");
-const productRouter = require("../Router/ProductRouter");
+const userRouter = require("./Router/UserRouter");
+const productRouter = require("./Router/ProductRouter");
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
 
-// Default route
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to E-commerce API" });
+app.use("/",(req,res)=>{
+    res.send("Api is running");
+})
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("db connected"))
+    .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-// MongoDB connect (runs once)
-mongoose
-  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("db connected"))
-  .catch((err) => console.log(err));
-
-// Export as serverless function
-module.exports = serverlessExpress({ app });
