c87617311ba891405d22eaab214de86e31fbc84f
parthpateltechein <parthpatel@techein.com>
2025-08-25 11:16:15 +0530

    change in app.js

--- app.js
+++ app.js
@@ -24,9 +24,7 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log("db connected"))
     .catch((err) => console.log(err));
 
-// const PORT = process.env.PORT || 7000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-module.exports = app;
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
